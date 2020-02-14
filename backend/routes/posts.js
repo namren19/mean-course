6742adae9a73ab64718466b1e683dc939e717b91
Norman Uy <namren19@gmail.com>
2020-02-14 16:17:09 +0800

    Section 11 Using controllers is done

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const Post = require('../models/post');
+
 const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
@@ -27,130 +27,12 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post(
-  "",
-  checkAuth,
-  multer({ storage: storage}).single('image'), (req, res, next) => {
-  const url = req.protocol + '://' + req.get("host");
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-  post.save()
-  .then((createdPost) => {
-    console.log(createdPost);
-    res.status(201).json({
-      message: 'Post Added Successfully',
-      post: {
-        // ...createdPost, // spread operator it will copy the js object
-        id: createdPost._id,
-        title: createdPost.title,
-        content: createdPost.content,
-        imagePath: createdPost.imagePath
-      }
-  });
+const PostController = require('../controllers/post');
 
-  })
-  .catch(error => {
-    res.status(500).json({
-      message: 'Creating a post failed!'
-    });
-  });
-});
-
-router.get("", (req, res, next) => {
-  // console.log(req.query);
-  const pageSize = +req.query.pagesize; // make a + string a integer
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery
-    .skip(pageSize * (currentPage - 1))
-    .limit(pageSize);
-  }
-  postQuery.then((documents) => {
-      //console.log(documents);
-      fetchedPosts = documents;
-      return Post.count();
-    })
-    .then((count) => {
-      res.status(200).json({
-        message: 'POST fetch successfully',
-        posts: fetchedPosts,
-        maxPosts: count
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: 'Fetching post failed!'
-      });
-    });
-});
-
-router.delete(
-  "/:id",
-  checkAuth,
-  (req, res, next) => {
-  console.log(req.params.id);
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then((result) => {
-    if (result.n > 0) {
-      res.status(200).json({ message: "Post deleted" });
-    } else {
-      res.status(401).json({ message: "Not Authorized!" });
-    }
-  })
-  .catch(error => {
-    res.status(500).json({ message: 'Deleting post failed!' });
-  });
-
-});
-
-router.put(
-  "/:id",
-  checkAuth,
-  multer({ storage: storage}).single('image'), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-  if (req.file) {
-    const url = req.protocol + '://' + req.get("host");
-    imagePath = url + "/images/" + req.file.filename
-  }
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath,
-    creator: req.userData.userId
-  });
-
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-  .then((result) => {
-    if (result.nModified > 0) {
-      res.status(200).json({ message: "Successfully updated!" });
-    } else {
-      res.status(401).json({ message: "Not Authorized" });
-    }
-  })
-  .catch((error) => {
-    res.status(500).json({ message: "Couldn't update post" });
-  });
-});
-
-router.get('/:id', (req, res, next) => {
-    Post.findById(req.params.id)
-    .then((post) => {
-        if (post) {
-            res.status(200).json(post);
-        } else {
-          res.status(404).json({ message: "Post not found" });
-        }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Fetching post failed!' });
-    });
-
-});
+router.post("", checkAuth, multer({ storage: storage}).single('image'), PostController.createPost);
+router.get("", PostController.getPosts);
+router.delete("/:id", checkAuth, PostController.deletePost);
+router.put("/:id", checkAuth, multer({ storage: storage}).single('image'), PostController.updatePost);
+router.get('/:id', PostController.getPost);
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,73 +1,10 @@
 const express = require('express');
-const brcypt = require('bcrypt')
-const jwt = require('jsonwebtoken');
-
 const router = express.Router();
-const User = require('../models/user');
 
-router.post("/signup", (req, res, next) => {
-  brcypt.hash(req.body.password, 10)
-  .then(hash => {
-    const user = new User({
-      email: req.body.email,
-      password: hash
-    });
-      user.save()
-      .then(result => {
-        res.status(201).json({
-          message: "User created!",
-          result: result
-        });
-      })
-    .catch(err => {
-      res.status(500).json({
-        message: "Invalid credentials"
-      });
-    });
-  })
-
-});
+const UserController = require('../controllers/user');
 
 
-router.post("/login", (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-  .then(user => {
-    console.log(user);
-    if (!user) {
-      return res.status(401).json({
-        message: "Auth Failed"
-      });
-    }
-    fetchedUser = user;
-    return brcypt.compare(req.body.password, user.password); // return must be use if there is another code to run after this
-  })
-  .then(result => {
-  //  console.log(result);
-      if (!result) {
-        return res.status(401).json({
-          message: "Auth Failed"
-        });
-      }
-
-  const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
-    'norman_will_be_success',
-    { expiresIn: "1h" }
-    );
-    // console.log(token);
-    res.status(200).json({
-      token: token,
-      expiresIn: 3600,
-      userId: fetchedUser._id
-    });
-  })
-  .catch(err => {
-    // console.log(err);
-    return res.status(401).json({
-      message: "Invalid Credentials"
-    });
-  });
-});
-
+router.post("/signup", UserController.createUser);
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
